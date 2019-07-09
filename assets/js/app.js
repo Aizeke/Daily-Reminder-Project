e950968a36015d4116a65d4ae6d413c1f6813c34
Aizeke <iguido97@icloud.com>
2019-07-08 20:55:06 -0700

    Added Nav, Jumbotron, Carousel, NewsAPI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,57 @@
+$(document).ready(function(){
+    $('#carouselSlideShow').carousel({
+        interval: 3000
+    });
 
-var twitterURL = 'https://api.twitter.com/1.1/search/tweets.json?q=from%3Atwitterdev&result_type=mixed&count=2'
+    $('.prev').on('click', function(){
+        $('#carouselSlideShow').carousel('prev')
+    });
 
-axios.get(twitterURL)
-.then(function(response){
-    console.log(response)
-})
+    $(".next").click(function(){
+        $("#carouselSlideShow").carousel("next");
+    });
+});
+
+// ===============================================================================================
+// ===============================================================================================
+
+ // bbs top 10 headlines
+ var queryURL = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=22aaeb67a2434db9922c8696bbd7c2e6";
+
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ })
+   // We store all of the retrieved data inside of an object called "response"
+   .then(function(response) {
+
+     // Log the queryURL
+     console.log(queryURL);
+
+     // Log the resulting object
+     console.log(response);
+
+     
+   });
+
+       // any topic the user is interested in, here im using california for an example
+       var topic = "california";
+       var queryURL = "https://newsapi.org/v2/top-headlines?q=" + topic + "&apiKey=22aaeb67a2434db9922c8696bbd7c2e6";
+   
+       $.ajax({
+         url: queryURL,
+         method: "GET"
+       })
+         // We store all of the retrieved data inside of an object called "response"
+         .then(function(response) {
+   
+           // Log the queryURL
+           console.log(queryURL);
+   
+           // Log the resulting object
+           console.log(response);
+   
+           
+         });
+         // ===============================================================================================
+         // ===============================================================================================
